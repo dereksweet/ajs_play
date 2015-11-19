a98d81092532b7f613f30e1bc6c31d679153161c
Derek Sweet <derek@arizonabay.com>
2015-11-18 23:31:27 -0700

    add_testing: Still can't get angular testing to work, WIP

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,17 @@
-require('phantomjs');
-//require('angular');
+var jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('<html><head><script></script></head><body></body></html>');
+global.window = global.document.parentWindow;
+
+//global.window.mocha = {};
+//global.window.beforeEach = beforeEach;
+//global.window.afterEach = afterEach;
+
+//global.angular = require('angular/angular');
+//require('angular-mocks');
+
+//global.inject = angular.mock.inject;
+//global.ngModule = angular.mock.module;
 
 // Chai for assertions.
 var chai = require('chai');
