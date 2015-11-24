fb17161b1b0f76c67d18044810cf5868ccd7b12f
Derek Sweet <derek@petrofeed.com>
2015-11-24 13:41:22 -0700

    Finished off route testing

--- test/angular/controllers/pages/routes_spec.js
+++ test/angular/controllers/pages/routes_spec.js
@@ -4,21 +4,159 @@ require('app/assets/javascripts/angular/controllers/pages/routes.js.erb');
 
 describe('routesApp', function () {
 
-    beforeEach(angular.mock.module("routesApp"));
+  beforeEach(angular.mock.module("routesApp"));
 
-    describe('routesCtrl', function () {
+  describe('routesConfig', function () {
+    it('should configure the routes properly', inject(function ($route) {
+      expect($route.routes['/'].controller).to.equal('homeCtrl');
+      expect($route.routes['/'].templateUrl).to.equal(asset_paths['pages/routes/home.html']);
 
-        mockController = function ($controller, $rootScope) {
-            mockScope = $rootScope.$new();
-            controller = $controller("routesCtrl", {
-                $scope: mockScope
-            });
-        };
+      expect($route.routes['/about'].controller).to.equal('aboutCtrl');
+      expect($route.routes['/about'].templateUrl).to.equal(asset_paths['pages/routes/about.html']);
 
-        beforeEach(angular.mock.inject(mockController));
+      expect($route.routes['/contact'].controller).to.equal('contactCtrl');
+      expect($route.routes['/contact'].templateUrl).to.equal(asset_paths['pages/routes/contact.html']);
 
-        it('default active to "home"', function () {
-            expect(controller.active).to.equal('home');
+      expect($route.routes['/eager/:extra*'].controller).to.equal('eagerCtrl');
+      expect($route.routes['/eager/:extra*'].templateUrl).to.equal(asset_paths['pages/routes/eager.html']);
+
+      expect($route.routes[null].controller).to.equal('notFoundCtrl');
+      expect($route.routes[null].templateUrl).to.equal(asset_paths['pages/routes/routeNotFound.html']);
+    }));
+  });
+
+  describe('routesCtrl', function () {
+
+    mockController = function ($controller, $rootScope) {
+      mockScope = $rootScope.$new();
+      controller = $controller("routesCtrl", {
+          $scope: mockScope
+      });
+    };
+
+    beforeEach(angular.mock.inject(mockController));
+
+    it('default active to "home"', function () {
+      expect(controller.active).to.equal('home');
+    });
+
+    describe('this.isActive', function () {
+
+      it('returns "active" if the passed in page is active, otherwise ""', function() {
+        controller.active = 'about';
+        expect(controller.isActive('about')).to.equal('active');
+        expect(controller.isActive('home')).to.equal('');
+      });
+
+    });
+  });
+
+
+
+  // homeCtrl has no functionality so x it out for now
+  xdescribe('homeCtrl', function () {
+
+    mockController = function ($controller, $rootScope) {
+      mockScope = $rootScope.$new();
+      controller = $controller("homeCtrl", {
+        $scope: mockScope
+      });
+    };
+
+    beforeEach(angular.mock.inject(mockController));
+  });
+
+
+
+  // aboutCtrl has no functionality so x it out for now
+  xdescribe('aboutCtrl', function () {
+
+    mockController = function ($controller, $rootScope) {
+      mockScope = $rootScope.$new();
+      controller = $controller("aboutCtrl", {
+        $scope: mockScope
+      });
+    };
+
+    beforeEach(angular.mock.inject(mockController));
+  });
+
+
+
+  describe('contactCtrl', function () {
+
+    describe('the subject passed in is "learn"', function() {
+      mockController = function ($controller, $rootScope) {
+        mockScope = $rootScope.$new();
+        mockRouteParams = { 'subject': 'learn' };
+        controller = $controller("contactCtrl", {
+          $scope: mockScope,
+          $routeParams: mockRouteParams
         });
-    })
+      };
+
+      beforeEach(angular.mock.inject(mockController));
+
+      it('should set showLearn true', function () {
+        expect(mockScope.showLearn).to.be.ok;
+      });
+    });
+
+    describe('the subject passed in is "learn"', function() {
+      mockController = function ($controller, $rootScope) {
+        mockScope = $rootScope.$new();
+        mockRouteParams = { 'subject': 'complain' };
+        controller = $controller("contactCtrl", {
+          $scope: mockScope,
+          $routeParams: mockRouteParams
+        });
+      };
+
+      beforeEach(angular.mock.inject(mockController));
+
+      it('should set showLearn true', function () {
+        expect(mockScope.showComplain).to.be.ok;
+      });
+    });
+  });
+
+
+
+  describe('eagerCtrl', function () {
+
+    mockController = function ($controller, $rootScope) {
+      mockScope = $rootScope.$new();
+      mockRouteParams = { 'extra': '12/34/56' };
+      controller = $controller("eagerCtrl", {
+        $scope: mockScope,
+        $routeParams: mockRouteParams
+      });
+    };
+
+    beforeEach(angular.mock.inject(mockController));
+
+    it('should set extra to the routeParams extra', function () {
+      expect(mockScope.extra).to.equal('12/34/56');
+    });
+  });
+
+
+
+  describe('notFoundCtrl', function () {
+
+    mockController = function ($controller, $rootScope) {
+      mockScope = $rootScope.$new();
+      mockLocation = { path: sinon.stub().returns('/routes/i_blow_goats') };
+      controller = $controller("notFoundCtrl", {
+        $scope: mockScope,
+        $location: mockLocation
+      });
+    };
+
+    beforeEach(angular.mock.inject(mockController));
+
+    it('should set attemptedLocation to the $location page', function() {
+      expect(mockScope.attemptedLocation).to.equal('/routes/i_blow_goats');
+    });
+  });
 });
--- test/mock_asset_paths.js
+++ test/mock_asset_paths.js
@@ -2,3 +2,10 @@ global.asset_paths = { };
 
 asset_paths['pages/animate/derek-sweet.html'] = 'pages/animate/derek-sweet.html';
 asset_paths['pages/animate/kelsey-sweet.html'] = 'pages/animate/kelsey-sweet.html';
+
+asset_paths['pages/routes/home.html'] = 'pages/routes/home.html';
+asset_paths['pages/routes/about.html'] = 'pages/routes/about.html';
+asset_paths['pages/routes/contact.html'] = 'pages/routes/contact.html';
+asset_paths['pages/routes/eager.html'] = 'pages/routes/eager.html';
+asset_paths['pages/routes/home.html'] = 'pages/routes/home.html';
+asset_paths['pages/routes/routeNotFound.html'] = 'pages/routes/routeNotFound.html';
