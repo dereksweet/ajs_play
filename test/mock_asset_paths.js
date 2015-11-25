b32126279c6d67a315ed946147c06413d2da7cbb
Derek Sweet <derek@petrofeed.com>
2015-11-25 08:30:26 -0700

    Make mock_asset_paths.js as robust as asset_paths.js

--- test/mock_asset_paths.js
+++ test/mock_asset_paths.js
@@ -1,19 +1,13 @@
+var glob = require('glob-fs')();
+var fs = require('fs');
+var templates = glob.readdirSync('app/assets/templates/**/*');
+
 global.asset_paths = { };
 
-asset_paths['pages/animate/derek-sweet.html'] = 'pages/animate/derek-sweet.html';
-asset_paths['pages/animate/kelsey-sweet.html'] = 'pages/animate/kelsey-sweet.html';
-
-asset_paths['pages/routes/home.html'] = 'pages/routes/home.html';
-asset_paths['pages/routes/about.html'] = 'pages/routes/about.html';
-asset_paths['pages/routes/contact.html'] = 'pages/routes/contact.html';
-asset_paths['pages/routes/eager.html'] = 'pages/routes/eager.html';
-asset_paths['pages/routes/home.html'] = 'pages/routes/home.html';
-asset_paths['pages/routes/routeNotFound.html'] = 'pages/routes/routeNotFound.html';
-
-asset_paths['pages/uirouter/multiple.html'] = 'pages/uirouter/multiple.html';
-asset_paths['pages/uirouter/nested.state1.viewA.html'] = 'pages/uirouter/nested.state1.viewA.html';
-asset_paths['pages/uirouter/nested.state1.viewB.html'] = 'pages/uirouter/nested.state1.viewB.html';
-asset_paths['pages/uirouter/nested.state2.viewA.html'] = 'pages/uirouter/nested.state2.viewA.html';
-asset_paths['pages/uirouter/nested.state2.viewB.html'] = 'pages/uirouter/nested.state2.viewB.html';
-asset_paths['pages/uirouter/nested.html'] = 'pages/uirouter/nested.html';
-asset_paths['pages/uirouter/nested.list.html'] = 'pages/uirouter/nested.list.html';
+for (index in templates) {
+  template = templates[index];
+  if(fs.lstatSync(template).isFile()) {
+    trimmed_template = template.replace('app/assets/templates/', '');
+    asset_paths[trimmed_template] = trimmed_template;
+  }
+}
