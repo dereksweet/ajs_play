8c57a1a6bb057ea7ac10af954ccd03112562f3f0
Derek Sweet <derek@petrofeed.com>
2015-12-02 12:02:21 -0700

    fix stale element errors with sleeps and promises

--- integration/e2e/pages/routes.test.js
+++ integration/e2e/pages/routes.test.js
@@ -6,20 +6,36 @@ describe('routes', function () {
   });
 
   describe('ngRoute', function () {
+    // ngRoute seems to have issues with the elements not being loaded into the DOM super quick, so some browser.driver.sleep
+    //   commands have been included after the nav link clicks to ensure that angular has enough time to finish before
+    //   protractor tries to see if the header has changed. 100 milliseconds seems to be enough on my machine, if you
+    //   experience any stale element errors while testing please let me know with a github issue.
     it('should be able to navigate through all the tabs', function () {
       expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Home');
-      element(by.id('about_link')).click();
-      expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('About');
-      element(by.id('contact_link')).click();
-      expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Contact');
-      element(by.id('contact_learn_link')).click();
-      expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Contact');
-      element(by.id('contact_complain_link')).click();
-      expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Contact');
-      element(by.id('eager_link')).click();
-      expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Eager');
-      element(by.id('undefined_link')).click();
-      expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Page Not Found');
+      element(by.id('about_link')).click().then(function() {
+        browser.driver.sleep(100);
+        expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('About');
+      });
+      element(by.id('contact_link')).click().then(function() {
+        browser.driver.sleep(100);
+        expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Contact');
+      });
+      element(by.id('contact_learn_link')).click().then(function() {
+        browser.driver.sleep(100);
+        expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Contact');
+      });
+      element(by.id('contact_complain_link')).click().then(function() {
+        browser.driver.sleep(100);
+        expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Contact');
+      });
+      element(by.id('eager_link')).click().then(function() {
+        browser.driver.sleep(100);
+        expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Eager');
+      });
+      element(by.id('undefined_link')).click().then(function() {
+        browser.driver.sleep(100);
+        expect(element(by.id('routes_ng_view')).element(by.tagName('h1')).getText()).toEqual('Page Not Found');
+      });
     });
   });
 });
