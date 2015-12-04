f54c0b0c75934d9470798bf7ed7749ff2ee8586c
Derek Sweet <derek@arizonabay.com>
2015-12-03 21:09:31 -0700

    master: make animate controllerAs

--- app/assets/javascripts/angular/controllers/pages/animate.js
+++ app/assets/javascripts/angular/controllers/pages/animate.js
@@ -3,14 +3,15 @@ var controllerName = "animateCtrl";
 
 angular.module(moduleName, ['ngAnimate']);
 
-var animateCtrl = function ($scope) {
-  $scope.animate_on = false;
+var animateCtrl = function () {
+  var vm = this;
+  vm.animate_on = false;
 
-  $scope.people = [
+  vm.people = [
     { name: 'Derek Sweet', url: asset_paths['pages/animate/derek-sweet.html'] },
     { name: 'Kelsey Sweet', url: asset_paths['pages/animate/kelsey-sweet.html'] }
   ];
-  $scope.selected_person = "";
+  vm.selected_person = "";
 };
-animateCtrl.$inject = ['$scope'];
+animateCtrl.$inject = [];
 angular.module(moduleName).controller(controllerName, animateCtrl);
--- test/angular/controllers/pages/animate_spec.js
+++ test/angular/controllers/pages/animate_spec.js
@@ -8,28 +8,27 @@ describe('animateApp', function () {
 
   describe('animateCtrl', function () {
 
-    mockController = function ($controller, $rootScope) {
-      mockScope = $rootScope.$new();
+    mockController = function ($controller) {
       controller = $controller("animateCtrl", {
-        $scope: mockScope
+
       });
     };
 
     beforeEach(angular.mock.inject(mockController));
 
     it('should default animate_on to false', function () {
-      expect(mockScope.animate_on).to.equal(false);
+      expect(controller.animate_on).to.equal(false);
     });
 
     it('should create the "people" object', function () {
-      expect(mockScope.people[0]['name']).to.equal('Derek Sweet');
-      expect(mockScope.people[0]['url']).to.equal('pages/animate/derek-sweet.html');
-      expect(mockScope.people[1]['name']).to.equal('Kelsey Sweet');
-      expect(mockScope.people[1]['url']).to.equal('pages/animate/kelsey-sweet.html');
+      expect(controller.people[0]['name']).to.equal('Derek Sweet');
+      expect(controller.people[0]['url']).to.equal('pages/animate/derek-sweet.html');
+      expect(controller.people[1]['name']).to.equal('Kelsey Sweet');
+      expect(controller.people[1]['url']).to.equal('pages/animate/kelsey-sweet.html');
     });
 
     it('should default selected_person to empty string', function () {
-      expect(mockScope.selected_person).to.equal('');
+      expect(controller.selected_person).to.equal('');
     });
 
   })
