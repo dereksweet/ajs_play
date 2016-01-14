d9014a99ceee0123eaf735f0fa96862843cbe7a8
Derek Sweet <derek@petrofeed.com>
2016-01-14 13:24:02 -0700

    Finished off specs for sweet-list directive

--- test/angular/directives/sweet-list.directive.spec.js
+++ test/angular/directives/sweet-list.directive.spec.js
@@ -1,12 +1,25 @@
 require('test/test_helper.js');
 require('app/assets/javascripts/angular/directives/sweet-list.directive');
-require('app/assets/javascripts/angular/controllers/pages/directives.controller');
 
-var $compile, $scope, $filter, $controller, $controllerProvider, $directiveElem;
+var $compile, $scope, $filter, $controller, $directiveElem;
 
 describe('sweet-list directive', function () {
   beforeEach(function(){
-    angular.mock.module(moduleName);
+    angular.mock.module(moduleName, function($controllerProvider) {
+      $controllerProvider.register('DirectivesController', function() {
+        this.products = [
+          { name: "Apples", price: 1.20 },
+          { name: "Bananas", price: 2.42 },
+          { name: "Pears", price: 2.03 }
+        ];
+
+        this.incrementPrices = function() {
+          for (var i = 0; i < this.products.length; i++) {
+            this.products[i].price++;
+          }
+        }
+      })
+    });
 
     inject(function(_$compile_, _$filter_, _$controller_, $rootScope){
       $compile = _$compile_;
@@ -21,20 +34,13 @@ describe('sweet-list directive', function () {
     $directiveElem = compiledElement;
   });
 
-  it.only('shoud be true', function () {
-    console.log($directiveElem.html());
-    expect(true).to.equal(true);
+  it('should set the html of the directive to the unordered list of products', function () {
+    expect($directiveElem.html()).to.equal("<ul><li>$1.20</li><li>$2.42</li><li>$2.03</li></ul>");
   });
-  //it('should display the current Date in the element text', function () {
-  //  expect($directiveElem.text()).to.equal($filter('date')(currentDate, 'medium'));
-  //});
-  //
-  //it('should keep the displayed date updated', function () {
-  //  expect($directiveElem.text()).to.equal($filter('date')(currentDate, 'medium'));
-  //  var newDate = new Date();
-  //  newDate.setDate(currentDate.getDate() + 1);
-  //  tk.travel(newDate);
-  //  $scope.$digest();
-  //  expect($directiveElem.text()).to.equal($filter('date')(newDate, 'medium'));
-  //});
+
+  it('should update the html when listed objects change', function () {
+    $directiveElem.controller('sweetList').incrementPrices();
+    $scope.$digest();
+    expect($directiveElem.html()).to.equal("<ul><li>$2.20</li><li>$3.42</li><li>$3.03</li></ul>");
+  });
 });
